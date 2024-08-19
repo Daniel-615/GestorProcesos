891bfd13ce122043a836fa5667a851f483031b2b
Daniel-615 <suyanagel615@gmail.com>
2024-08-19 12:21:02 -0600

    feature graph-stack, manage-process, process

---
+++ src/GestorProceso.py
@@ -0,0 +1,50 @@
+from Pila import Pila
+from Proceso import Proceso
+import concurrent.futures
+import os
+
+class GestorProcesos:
+    def __init__(self):
+        self.pila_procesos = Pila()
+        
+    def agregar_proceso(self, proceso):
+        self.pila_procesos.apilar(proceso)
+
+    def ejecutar_procesos(self, max_cores):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_cores) as executor:
+            futuros = []
+            while not self.pila_procesos.esta_vacia():
+                proceso = self.pila_procesos.desapilar()
+                if proceso:
+                    futuro = executor.submit(proceso.ejecutar)
+                    futuros.append(futuro)
+            
+            for futuro in concurrent.futures.as_completed(futuros):
+                futuro.result()
+
+    def consulta_cores(self):
+        self.cores = os.cpu_count()
+        print(f"Tienes {self.cores} cores")
+        cores = int(input("¿Cuántos cores deseas utilizar? "))
+        return cores
+    
+    def visualizar(self):
+        self.pila_procesos.visualizar_pila()
+
+# Ejemplo de uso
+gestor = GestorProcesos()
+cores = gestor.consulta_cores()
+
+# Agregar procesos
+for n in range(cores):
+    gestor.agregar_proceso(Proceso("alta", f"Proceso {n+1}", "tipo1"))
+
+# Visualizar antes de procesar
+gestor.visualizar()
+
+# Ejecutar procesos con el número de cores especificado
+gestor.ejecutar_procesos(max_cores=cores)
+
+# Visualizar después de procesar
+# Puedes descomentar esto si necesitas visualizar la pila después
+# gestor.visualizar()
---
+++ src/Proceso.py
@@ -0,0 +1,19 @@
+#hilos
+import time
+class Proceso:
+    contador=0
+    def __init__(self,prioridad,proceso,tipo_proceso):
+        self.contador+=1
+        self.prioridad=prioridad
+        self.proceso=proceso
+        self.tipo_proceso=tipo_proceso
+    def ejecutar(self):
+        if self.prioridad=="alta":
+            self.tiempo=2
+        if self.prioridad=="media":
+            self.tiempo=4
+        if self.prioridad=="baja":
+            self.tiempo=8
+        print(f"ejecutando proceso con prioridad {self.prioridad} tipo: ({self.tipo_proceso}) ")
+        time.sleep(self.tiempo)
+        print(f"Proceso {self.proceso} completado en {self.tiempo} segundos")
